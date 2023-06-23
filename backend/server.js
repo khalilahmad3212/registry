69b55fcc479a88ca2dfccea34d6123efac3f9781
khalilahmad3212 <arzishjani1613@gmail.com>
2023-06-23 17:45:22 +0500

    build

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const path = require('path')
 const { default: mongoose } = require('mongoose')
 
 const app = express()
